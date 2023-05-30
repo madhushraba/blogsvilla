6215f36948830b43da3cd83c16dc080893acedc6
Madhushraba <madhushraba21das@gmail.com>
2023-05-31 04:41:53 +0530

    headerjsx added

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Topbar from './topbar/Topbar'
 import Home from './pages/home/Home'
+import Header from './header/Header'
 
 const Landingpage = () => {
   return (
     <div>
         <div className='body'>
       {/* <Topbar/> */}
+      <Header/>
       <Home/>
     </div>
     </div>
